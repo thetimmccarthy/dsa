83e48a37a74ee5b10bbc33c9de608a84c13e6ed3
Tim McCarthy <thetimmccarthy@gmail.com>
2022-04-10 14:41:13 -0400

    cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,9 @@ int main () {
     AVLTree* tree = new AVLTree();
     
     tree->insert(4);
-    // std::cout << tree->root->value <<std::endl;
     tree->insert(5);
-    // std::cout << tree->root->right->value <<std::endl;
     tree->insert(6);
-    
     tree->breadthFirstTraversal();
-
-    // tree->root = new TreeNode(4);
-    // tree->root->right = new TreeNode(6);
-    // tree->root->left = new TreeNode(2);
-    
-    // TreeNode* tmp = tree->find(2);
-    // std::cout << tmp->value << " " << tmp << std::endl;
 
     delete tree;
     tree = NULL;
